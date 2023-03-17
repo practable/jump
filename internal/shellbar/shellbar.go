75c1e9fb225bf4c44a8eaac72402b39022dac8b7
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-03-17 15:44:46 +0000

    shellbar (chore): remove stale code

--- internal/shellbar/shellbar.go
+++ internal/shellbar/shellbar.go
@@ -235,13 +235,9 @@ func Shellbar(closed <-chan struct{}, parentwg *sync.WaitGroup, config Config) {
 
 	topics.directory = make(map[string][]clientDetails)
 
-	clientActionsChan := make(chan clientAction)
-
-	wg.Add(2)
+	wg.Add(1)
 
 	go handleConnections(closed, &wg, config)
-
-	go handleClients(closed, &wg, &topics, clientActionsChan)
 
 	wg.Wait()
 
@@ -289,103 +285,6 @@ func handleConnections(closed <-chan struct{}, parentwg *sync.WaitGroup, config 
 	wg.Wait()
 	parentwg.Done()
 	log.Trace("handleConnections is done")
-}
-
-func handleClients(closed <-chan struct{}, wg *sync.WaitGroup, topics *topicDirectory, clientActionsChan chan clientAction) {
-
-	defer func() {
-
-		wg.Done()
-
-		log.WithFields(log.Fields{
-			"func": "HandleClients",
-			"verb": "closed",
-		}).Trace("HandleClients closed")
-
-	}()
-
-	for {
-		select {
-		case <-closed:
-			return
-		case request := <-clientActionsChan:
-
-			log.WithField("clientAction", request).Trace("handleClients")
-
-			if request.action == clientAdd {
-
-				addClientToTopic(topics, request.client)
-
-			} else if request.action == clientDelete {
-				deleteClientFromTopic(topics, request.client)
-
-			}
-		}
-	}
-}
-
-func addClientToTopic(topics *topicDirectory, client clientDetails) {
-
-	_, exists := topics.directory[client.topic]
-
-	if !exists {
-		topics.Lock()
-		topics.directory[client.topic] = []clientDetails{client}
-		topics.Unlock()
-
-		log.WithFields(log.Fields{
-			"topic":  client.topic,
-			"client": client,
-			"action": clientAdd,
-			"verb":   "add",
-			"count":  1,
-		}).Debug("Added first client to new topic")
-
-	} else {
-		topics.Lock()
-		topics.directory[client.topic] = append(topics.directory[client.topic], client)
-		count := len(topics.directory[client.topic])
-		topics.Unlock()
-
-		log.WithFields(log.Fields{
-			"topic":  client.topic,
-			"client": client,
-			"action": clientAdd,
-			"verb":   "add",
-			"count":  count,
-		}).Debug("Added client to existing topic")
-
-	}
-}
-
-func deleteClientFromTopic(topics *topicDirectory, client clientDetails) {
-
-	_, exists := topics.directory[client.topic]
-	if exists {
-		topics.Lock()
-		existingClients := topics.directory[client.topic]
-		topics.directory[client.topic] = filterClients(existingClients, client)
-		count := len(topics.directory[client.topic])
-		topics.Unlock()
-
-		log.WithFields(log.Fields{
-			"topic":  client.topic,
-			"client": client,
-			"action": clientDelete,
-			"verb":   "delete",
-			"count":  count,
-		}).Debug("Deleting client from existing topic")
-
-	} else {
-
-		log.WithFields(log.Fields{
-			"topic":  client.topic,
-			"client": client,
-			"action": clientDelete,
-			"verb":   "delete",
-			"count":  0,
-		}).Debug("Ignoring: can't delete client from non-existent topic")
-	}
 }
 
 // Hub maintains the set of active clients and broadcasts messages to the
